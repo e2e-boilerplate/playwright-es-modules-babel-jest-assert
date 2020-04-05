afe6d75680cc8295d889bc4191e44710000313a2
Girma Nigusse <xgirma@gmail.com>
2020-04-05 10:16:26 -0400

    updates

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,5 @@ module.exports = {
   testEnvironment: "node",
   testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[tj]s?(x)"],
   testPathIgnorePatterns: ["/node_modules/"],
+  setupFilesAfterEnv: ["./jest.setup.js"],
 };
